4178eef10c103d9e1088c147f9abc68278f74217
Kamil Gołdyn <1996kamil07@gmail.com>
2023-05-11 10:24:10 +0200

    calculateSpeed method added

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,13 @@ public class Main {
     private static final DecimalFormat df = new DecimalFormat("0.00");
     public static void main(String[] args) {
 
+
         System.out.println(fahrenheitToCelsius(212));
         System.out.println(inchesToMeters(1000));
         System.out.println(sumOfDigits(1000));
         minutesToYearsAndDays(3456789);
         System.out.println(calculateBMI(75,1.75));
+        System.out.println(calculateSpeed(2500,5,56,23));
 
     }
 
@@ -47,5 +49,14 @@ public class Main {
     //calculating BMI index based on given weight and height
     public static String calculateBMI(int weight, double height) {
         return df.format((weight / Math.pow(height,2)));
+    }
+
+    //calculating average speed based od covered distance, and total time it took to beat
+    public static double calculateSpeed(double distanceInMeters, int hours, double minutes, double seconds) {
+        double distanceInKm = distanceInMeters / 1000;
+        double totalTime = (hours + (minutes/60) + (seconds/60/60));
+        double speedCalculate = distanceInKm / totalTime;
+        System.out.println("You beat " + distanceInKm + "kms in " + df.format(totalTime) + " hours. Average speed equals: ");
+        return speedCalculate;
     }
 }

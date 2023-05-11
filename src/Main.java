1ea20199ac6dabd23ad16557b5c36de19ed2a21e
Kamil Gołdyn <1996kamil07@gmail.com>
2023-05-11 08:41:46 +0200

    inchedToMeters method added

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(fahrenheitToCelsius(212));
-
+        System.out.println(inchesToMeters(1000));
     }
+
     //converting temperature from Fahrenheit to Celsius degrees.
     public static float fahrenheitToCelsius(float temp) {
         return ((temp-32)*5/9);
+    }
+
+    //converting inches to meters
+    public static double inchesToMeters(double inches) {
+        return inches*0.0254;
     }
 }

59f12e810f6a0ccf0c42a6017e14737ab73b71de
Kamil Gołdyn <1996kamil07@gmail.com>
2023-05-11 09:38:45 +0200

    minutesToYearsAndDays method added

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println(fahrenheitToCelsius(212));
         System.out.println(inchesToMeters(1000));
         System.out.println(sumOfDigits(1000));
+        minutesToYearsAndDays(3456789);
 
     }
 
@@ -23,11 +24,18 @@ public class Main {
 
         if(number > 0 && number <= 1000) {
             for(int i = number; i > 0; i = i/10) {
-            sum += i % 10;
+            sum += i%10;
             }
+        } else {
+            System.out.println("Given number must be bigger than 0 and smaller than 1001!");
         }
         return sum;
     }
 
-
+    //converting minutes to years and days
+    public static void minutesToYearsAndDays(int minutes) {
+        long years = minutes/60/24/365;
+        long days = ((minutes/60/24) % 365);
+        System.out.println(minutes + " minutes equals about " + years + " years, and " + days + " days.");
+    }
 }
